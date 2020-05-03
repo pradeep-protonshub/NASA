8b50db094c8b8baf5c9e7778a2cb699a0e9d12ca
Pradeep Agrawal <pradeep.agrawal@protonshub.com>
2020-05-03 18:19:06 +0530

    added files

---
+++ SRC/Components/landingPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+export default class LandingPage extends React.Component{
+    render() {
+        return (
+            <View>
+                <Text>
+                    hello landing page
+                </Text>
+            </View>
+        )
+    }
+}
